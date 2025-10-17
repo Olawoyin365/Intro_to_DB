edc75d4e5adbe5d76234e2b3dddf9c71770500fe
Ibrahim Olawoyin <woyin365@gmail.com>
2025-10-17 09:41:37 +0000

    database that stores information about books, authors, customers, orders, and order details.

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -42,7 +42,7 @@ cursor.execute("""
 	CREATE TABLE Orders (
 		order_id INT PRIMARY KEY,
 		customer_id INT,
-		FOREIGN KEY customer_id REFERENCES Customers(customer_id),
+		FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
 		order_date DATE
 	)
 	""")
