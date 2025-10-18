d429304af9e5ab5e5d5be8a7669b34695cd6001d
Ibrahim Olawoyin <woyin365@gmail.com>
2025-10-18 00:54:08 +0000

    python script that handles connection to a database in 'MySQL server'.

---
+++ MySQLServer.py
@@ -0,0 +1,29 @@
+import mysql.connector
+from mysql.connector import Error
+ 
+db_name = "alx_book_store"
+ 
+try:
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="olawoyin",
+        password="1234",
+        database=db_name
+    )
+    if mydb.is_connected():
+        print(f"Database '{db_name}' already exists.")
+except Error as e:
+    if "Unknown database" in str(e):
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="olawoyin",
+            password="1234"
+        )
+        cursor = mydb.cursor()
+        cursor.execute(f"CREATE DATABASE {db_name}")
+        print(f"Database '{db_name}' created successfully.")
+    else:
+        print("Error:", e)
+finally:
+    if 'mydb' in locals() and mydb.is_connected():
+        mydb.close()
