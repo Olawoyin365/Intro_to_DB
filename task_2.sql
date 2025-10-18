0bda7d1e8b536c80a625097b2c1967af7e13f751
Ibrahim Olawoyin <woyin365@gmail.com>
2025-10-18 01:48:31 +0000

    a script that creates books, authtors, customers, orderdetails table in a mysql database

--- task_2.sql
+++ task_2.sql
@@ -10,25 +10,25 @@ mydb = mysql.connector.connect(
 cursor = mydb.cursor()
 
 cursor.execute("""
-	CREATE TABLE authors(
+	CREATE TABLE Authors(
 		author_id INT PRIMARY KEY,
 		author_name VARCHAR(215)
 	)
 	""")
 
 cursor.execute("""
-	CREATE TABLE books(
+	CREATE TABLE Books(
 		book_id INT PRIMAR KEY,
 		title VARCHAR(130),
 		author_id INT,
-		FOREIGN KEY author_id REFERENCES authors(author_id),
+		FOREIGN KEY (author_id) REFERENCES Authors(author_id),
 		price DOUBLE,
 		publication_date DATE
 	)
 	""")
 
 cursor.execute("""
-	CREATE TABLE customers(
+	CREATE TABLE Customers(
 		customer_id INT PRIMARY KEY,
 		customer_name VARCHAR(215),
 		email VARCHAR(215),
@@ -37,21 +37,21 @@ cursor.execute("""
 	""")
 
 cursor.execute("""
-	CREATE TABLE orders(
+	CREATE TABLE Orders(
 		order_id INT PRIMARY KEY,
 		customer_id INT,
-		FOREIGN KEY customer_id REFERENCES customers(customer_id),
+		FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
 		order_date DATE
 	)
 	""")
 
 cursor.execute("""
-	CREATE TABLE order_details(
+	CREATE TABLE Order_details(
 		orderdetailid INT PRIMARY KEY,
 		order_id INT,
-		FOREIGN KEY order_id REFERENCES orders(order_id),
+		FOREIGN KEY (order_id) REFERENCES Orders(order_id),
 		book_id INT,
-		FOREIGN KEY book_id REFERENCES books(book_id),
+		FOREIGN KEY (book_id) REFERENCES Books(book_id),
 		quantity DOUBLE
 	)
 	""")
