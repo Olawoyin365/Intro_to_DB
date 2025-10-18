402d446d6568f682cce6c5e06c4da57409deddd3
Ibrahim Olawoyin <woyin365@gmail.com>
2025-10-18 14:29:02 +0000

    a script that list all the tables in MySQL Server

--- task_3.sql
+++ task_3.sql
@@ -8,7 +8,7 @@ mydb = mysql.connector.connect(
 
 cursor = mydb.cursor()
 
-cursor.execute("USE alx_book_store")
+cursor.execute("USE alx_book_store;")
 
 cursor.execute("SHOW TABLES")
 for i in cursor:
