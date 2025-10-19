52409f539772ad54ddf994f27cb54821fbcffa6f
Ibrahim Olawoyin <woyin365@gmail.com>
2025-10-19 02:23:05 +0000

    a script that inserts a single row in the table customer (database alx_book_store) in MySQL server.

---
+++ task_5.sql
@@ -0,0 +1,16 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "Akbar@123$$$",
+    database = "alx_book_store"
+)
+
+cursor = mydb.cursor()
+
+cursor.execute("""
+	INSERT INTO Customers VALUES(
+		1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.")
+	""")
+
