697cdd6bf3f832e4221f647c5169a0b6b41ce35a
Ibrahim Olawoyin <woyin365@gmail.com>
2025-10-19 02:43:33 +0000

    a script that inserts multiple rows in the table customer (database alx_book_store) in MySQL server.

--- task_6.sql
+++ task_6.sql
@@ -10,7 +10,7 @@ mydb = mysql.connector.connect(
 cursor = mydb.cursor()
 
 cursor.execute("""
-	INSERT INTO customer customer_id, customer_name, email, address(
+	INSERT INTO customer VALUES(
 		1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave."),
 	(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.' ),
 	(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
